3a2c4d258a3db305129c5bf161f7d3cfc312cfd9
blu3tan <142602465+blu3tan@users.noreply.github.com>
2024-03-01 01:07:31 +0100

    Add append-node and print-size methods

--- list.mjs
+++ list.mjs
@@ -3,11 +3,27 @@ import Node from './node.mjs';
 class LinkedList {
 	constructor() {
 		this.head = null;
+		this.size = 0;
 	}
 
 	// Prepend a new node at the start
 	prepend(value) {
 		this.head = new Node(value, this.head);
+		this.size++;
+	}
+
+	// Append a new node at the end
+	append(value) {
+		if (!this.head) {
+			this.size++;
+			return (this.head = new Node(value, this.head));
+		}
+		let current = this.head;
+		while (current.next !== null) {
+			current = current.next;
+		}
+		current.next = new Node(value);
+		this.size++;
 	}
 
 	// Print a string with the nodes values
@@ -21,11 +37,17 @@ class LinkedList {
 		string += 'null';
 		console.log(string);
 	}
+
+	// Print total number of nodes
+	listSize() {
+		console.log(`This list has ${this.size} nodes`);
+	}
 }
 
 const testLinkedList = new LinkedList();
-testLinkedList.prepend(100);
-testLinkedList.prepend(200);
-testLinkedList.prepend(300);
+// testLinkedList.prepend(100);
+// testLinkedList.prepend(200);
+// testLinkedList.append(500);
+// testLinkedList.prepend(100);
 
-testLinkedList.toString();
+testLinkedList.listSize();
